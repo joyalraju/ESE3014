d646a3cec6850223e86ac456791f2cb60e998acb
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2020-02-03 23:26:22 -0500

    sample mean comparison with whole mean
    Write a script that calculates the mean of five samples of data from a vector of random data. Calculate the overall mean. Use a for loop to perform the calculations. For each iteration of the loop print out the intermediate results. Use an if..else control block to display the results depending on whether the mean of the samples is less than, greater than or equal to the overall mean.

---
+++ mean_compar.m
@@ -0,0 +1,13 @@
+a = randi(500,1,50)
+om=mean(a)
+for i=1:1:5
+  b=randi(randi(a),1,5)
+  m=mean(b)
+  if (m>om)
+    printf("mean is greater than overall mean \n")
+  elseif (m<om)
+     printf("mean is less than overall mean \n")
+    else
+      printf("mean is equa than overall mean \n")
+    end
+    end
