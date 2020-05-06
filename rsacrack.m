38851014dacdab6462e2f69e6efb0bc9c73e1a6e
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2020-05-05 22:03:04 -0400

    crack the rsa encryption
    crack rsa encryption if one knows the public key.

---
+++ rsacrack.m
@@ -0,0 +1,16 @@
+e=input("Enter e of public key: \n");
+n=input("Enter n of public key: \n");
+pq=factor(n);
+p=pq(1)
+q=pq(2)
+phi=(p-1)*(q-1)
+i=1;
+reminder=1;
+while reminder > 0
+  k=(phi*i)+1;
+   reminder=rem(k,e);
+    i=i+1;
+end
+d=k/e
+printf("Hence private key = (%d,%d) \n",n,d);
+printf("\n\n***CRACKED...!*** \n");
